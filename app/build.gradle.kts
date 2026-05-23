924ff0e45991dae864d9b80010d460a9edb88fc0
霍英杰 <qq1102770789@163.com>
2026-05-23 12:45:52 +0800

    chore: bump version to 2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.example.aiassistant"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
